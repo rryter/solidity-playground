a6a31ceb1f5a82726f4c6531077d6056ea506f7f
Reto Ryter <reto@twy.gmbh>
2020-11-05 13:33:39 +0100

    chore(ts-jest): disable @swc-node/jest for now

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,23 @@
 module.exports = {
   roots: ["<rootDir>/test"],
-  transform: {
-    "^.+\\.ts$": [
-      "@swc-node/jest",
-      {
-        target: "es2018",
-        module: "commonjs",
-        experimentalDecorators: true,
-        emitDecoratorMetadata: true
-      }
-    ]
+  preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+      isolatedModules: true
+    }
   },
+  //   transform: {
+  //     "^.+\\.ts$": [
+  //       "@swc-node/jest",
+  //       {
+  //         target: "es2018",
+  //         module: "commonjs",
+  //         experimentalDecorators: true,
+  //         emitDecoratorMetadata: true
+  //       }
+  //     ]
+  //   },
   testEnvironment: "node",
   setupFilesAfterEnv: ["./jest.setup.ts"],
   maxWorkers: 2,
